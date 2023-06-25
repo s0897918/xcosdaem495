4b90559385d79247d3ff2897c5513ed1918bfa11
s0897918 <s0897918@126.com>
2023-06-25 00:26:28 -0600

    sync

---
+++ dlrm/bench/cpu_dlrm_bench.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+mb_size=64 
+nbatches=1 
+
+
+#M3 setup
+arch_m3_mlp_lys=35
+arch_m3_emb_tbs=1800
+arch_m3_emb_size=8192
+
+# Don't change the following setup
+ncores=1 
+nsockets="0"
+numa_cmd="numactl --physcpubind=0-$((ncores-1)) -m $nsockets" #run on one socket, without HT
+dlrm_pt_bin="python dlrm_s_pytorch.py"
+print_freq=100
+rand_seed=727
+
+emb_size=128
+nindices=100
+interaction="dot"
+
+_args="--mini-batch-size="${mb_size}\
+" --num-batches="${nbatches}\
+" --use-m3-bot-mlp --use-m3-top-mlp --arch-m3-mlp-lys="${arch_m3_mlp_lys}\
+" --use-m3-emb --debug-m3-no --arch-m3-emb-tbs="${arch_m3_emb_tbs}\
+" --arch-m3-emb-size="${arch_m3_emb_size}\
+" --arch-sparse-feature-size="${emb_size}\
+" --num-indices-per-lookup="${nindices}\
+" --arch-interaction-op="${interaction}\
+" --numpy-rand-seed="${rand_seed}\
+" --print-freq="${print_freq}\
+" --print-time"
+
+# CPU Benchmarking
+echo "--------------------------------------------"
+echo "CPU Benchmarking - running on 1 core"
+echo "--------------------------------------------"
+
+cmd="$numa_cmd $dlrm_pt_bin $_args"
+
+echo $cmd
+eval $cmd
+
+
---
+++ dlrm/bench/gpu_dlrm_bench.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+ngpus="1"
+mb_size=64 
+nbatches=1 
+
+#M3 setup
+arch_m3_mlp_lys=35 #35
+arch_m3_emb_tbs=40
+arch_m3_emb_size=8192
+
+# Don't change the following setup
+dlrm_pt_bin="python dlrm_s_pytorch.py"
+
+print_freq=100
+rand_seed=727
+
+emb_size=128
+nindices=100
+interaction="dot"
+
+
+_args="--mini-batch-size="${mb_size}\
+" --num-batches="${nbatches}\
+" --use-m3-bot-mlp --use-m3-top-mlp --arch-m3-mlp-lys="${arch_m3_mlp_lys}\
+" --use-m3-emb --debug-m3-no --arch-m3-emb-tbs="${arch_m3_emb_tbs}\
+" --arch-m3-emb-size="${arch_m3_emb_size}\
+" --arch-sparse-feature-size="${emb_size}\
+" --num-indices-per-lookup="${nindices}\
+" --arch-interaction-op="${interaction}\
+" --numpy-rand-seed="${rand_seed}\
+" --print-freq="${print_freq}\
+" --print-time"
+
+# GPU Benchmarking
+echo "--------------------------------------------"
+echo "GPU Benchmarking - running on $ngpus GPUs"
+echo "--------------------------------------------"
+
+for _ng in $ngpus
+do
+    _gpus=$(seq -s, 0 $((_ng-1)))
+    cuda_arg="CUDA_VISIBLE_DEVICES=$_gpus"
+    echo "-------------------"
+    echo "Using GPUS: "$_gpus
+    echo "-------------------"
+    cmd="$cuda_arg $dlrm_pt_bin $_args --use-gpu"
+    echo $cmd
+    eval $cmd
+done
+
